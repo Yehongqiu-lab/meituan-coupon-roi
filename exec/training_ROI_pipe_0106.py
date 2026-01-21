63bb484d2b9c2235e77b1214b37b08d5b8676e96
Yehong Qiu <yehqiu616@gmail.com>
2026-01-21 12:10:05 -0800

    Add files via upload

---
+++ exec/training_ROI_pipe_0106.py
@@ -0,0 +1,238 @@
+import json
+import pickle
+import yaml
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+
+import lightgbm as lgb
+
+import os
+import sys
+root = os.path.dirname(os.getcwd())
+repo_root = os.path.join(root, "meituan-coupon-roi")
+src_path = os.path.join(repo_root, "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from train import *
+id_param = "1217"
+
+date = "0106"
+m_id = "m_7d"
+seg_param = [0, 0, 0]
+seg_id = "seg_000"
+
+params_yaml_path = os.path.join(repo_root, f"conf/params_ROI_{id_param}.yaml")
+
+model_saving_path_1 = os.path.join(repo_root, f"models/ROI/ROI_{date}_{m_id}_{seg_id}_fd1.pkl")
+perf_saving_path_1 = os.path.join(repo_root, f"experiment_results/ROI/perf_ROI_{date}_{m_id}_{seg_id}_fd1.txt")
+val_pred_saving_path_1 = os.path.join(repo_root, f"experiment_results/ROI/val_pred_ROI_{date}_{m_id}_{seg_id}_fd1.csv")
+
+model_saving_path_2 = os.path.join(repo_root, f"models/ROI/ROI_{date}_{m_id}_{seg_id}_fd2.pkl")
+perf_saving_path_2 = os.path.join(repo_root, f"experiment_results/ROI/perf_ROI_{date}_{m_id}_{seg_id}_fd2.txt")
+val_pred_saving_path_2 = os.path.join(repo_root, f"experiment_results/ROI/val_pred_ROI_{date}_{m_id}_{seg_id}_fd2.csv")
+
+model_saving_path_3 = os.path.join(repo_root, f"models/ROI/ROI_{date}_{m_id}_{seg_id}_fd3.pkl")
+perf_saving_path_3 = os.path.join(repo_root, f"experiment_results/ROI/perf_ROI_{date}_{m_id}_{seg_id}_fd3.txt")
+val_pred_saving_path_3 = os.path.join(repo_root, f"experiment_results/ROI/val_pred_ROI_{date}_{m_id}_{seg_id}_fd3.csv")
+
+print("Loading data for ROI training...")
+ROI_train_1, ROI_train_2, ROI_train_3, ROI_val_1, ROI_val_2, ROI_val_3 = \
+    load_ROI_training_3folds_data(repo_root,
+                                     pickle_path=f"conf/feature_selection/ROI_modeling/ROI_train_cols_{m_id}.pkl",
+                                     segment=True,
+                                     Price_limit_bin=seg_param[0],
+                                     Coupon_limit_bin=seg_param[1],
+                                     Expiry_span_bin=seg_param[2])
+
+
+X_train_1, y_train_1 = get_ROI_X_y_data(ROI_train_1, "st")
+X_train_2, y_train_2 = get_ROI_X_y_data(ROI_train_2, "st")
+X_train_3, y_train_3 = get_ROI_X_y_data(ROI_train_3, "st")
+
+W_train_1 = add_vanilla_weights(ROI_train_1, "label_same_user_st")
+W_train_2 = add_vanilla_weights(ROI_train_2, "label_same_user_st")
+W_train_3 = add_vanilla_weights(ROI_train_3, "label_same_user_st")
+
+X_val_1, y_val_1 = get_ROI_X_y_data(ROI_val_1, "st")
+X_val_2, y_val_2 = get_ROI_X_y_data(ROI_val_2, "st")
+X_val_3, y_val_3 = get_ROI_X_y_data(ROI_val_3, "st")
+
+W_val_1 = add_vanilla_weights(ROI_val_1, "label_same_user_st")
+W_val_2 = add_vanilla_weights(ROI_val_2, "label_same_user_st")
+W_val_3 = add_vanilla_weights(ROI_val_3, "label_same_user_st")
+
+
+lgb_train_1 = lgb.Dataset(
+    X_train_1, y_train_1, 
+    weight=W_train_1,
+    feature_name=X_train_1.columns.to_list(),
+    free_raw_data=True
+)
+lgb_val_1 = lgb.Dataset(
+    X_val_1, y_val_1, reference=lgb_train_1, weight=W_val_1, free_raw_data=True
+)
+
+lgb_train_2 = lgb.Dataset(
+    X_train_2, y_train_2,
+    weight=W_train_2, 
+    feature_name=X_train_2.columns.to_list(),
+    free_raw_data=True
+)
+lgb_val_2 = lgb.Dataset(
+    X_val_2, y_val_2, reference=lgb_train_2, weight=W_val_2, free_raw_data=True
+)
+
+lgb_train_3 = lgb.Dataset(
+    X_train_3, y_train_3, 
+    weight=W_train_3,
+    feature_name=X_train_3.columns.to_list(),
+    free_raw_data=True
+)
+lgb_val_3 = lgb.Dataset(
+    X_val_3, y_val_3, reference=lgb_train_3, weight=W_val_3, free_raw_data=True
+)
+
+with open(params_yaml_path, 'r') as f:
+    params = yaml.safe_load(f)
+
+print("starting ROI training with cross validation...")
+
+eval_result_1 = {}
+gbm_1 = lgb.train(
+    params,
+    lgb_train_1,
+    num_boost_round=10,
+    valid_sets=lgb_val_1,
+    callbacks=[lgb.record_evaluation(eval_result_1)]
+)
+
+"""gbm_1.save_model(model_saving_path_1, num_iteration=0)"""
+
+with open(model_saving_path_1, "wb") as fout:
+    pickle.dump(gbm_1, fout)
+
+recall_in_sample_1 = metric_individual_class_accuracy(y_true=y_train_1,
+                                                      y_pred=gbm_1.predict(X_train_1))
+recall_val_1 = metric_individual_class_accuracy(y_true=y_val_1,
+                                                y_pred=gbm_1.predict(X_val_1))
+trueNegRate_in_sample_1 = metric_individual_class_accuracy(y_true=y_train_1,
+                                                      y_pred=gbm_1.predict(X_train_1),
+                                                      which_class=0)
+trueNegRate_val_1 = metric_individual_class_accuracy(y_true=y_val_1,
+                                                      y_pred=gbm_1.predict(X_val_1),
+                                                      which_class=0)
+
+pred_dict = {"y_true": y_val_1, "y_pred": gbm_1.predict(X_val_1)}
+df = pd.DataFrame.from_dict(pred_dict)
+df.to_csv(val_pred_saving_path_1, index=False)
+
+perf_1 = {"eval_result": eval_result_1,
+          "recall_in_sample": recall_in_sample_1,
+          "recall_val": recall_val_1,
+          "trueNegRate_in_sample": trueNegRate_in_sample_1,
+          "trueNegRate_val": trueNegRate_val_1,
+          "thresholds for metric computation": [0.25, 0.5, 0.75]}
+with open(perf_saving_path_1, "w") as f:
+    f.write(str(perf_1))
+
+eval_result_2 = {}
+gbm_2 = lgb.train(
+    params,
+    lgb_train_2,
+    num_boost_round=10,
+    valid_sets=lgb_val_2,
+    callbacks=[lgb.record_evaluation(eval_result_2)]
+)
+"""gbm_2.save_model(model_saving_path_2, num_iteration=0)"""
+
+with open(model_saving_path_2, "wb") as fout:
+    pickle.dump(gbm_2, fout)
+
+recall_in_sample_2 = metric_individual_class_accuracy(y_true=y_train_2,
+                                                      y_pred=gbm_2.predict(X_train_2))
+recall_val_2 = metric_individual_class_accuracy(y_true=y_val_2,
+                                                y_pred=gbm_2.predict(X_val_2))
+
+trueNegRate_in_sample_2 = metric_individual_class_accuracy(y_true=y_train_2,
+                                                      y_pred=gbm_2.predict(X_train_2),
+                                                      which_class=0)
+trueNegRate_val_2 = metric_individual_class_accuracy(y_true=y_val_2,
+                                                      y_pred=gbm_2.predict(X_val_2),
+                                                      which_class=0)
+
+pred_dict = {"y_true": y_val_2, "y_pred": gbm_1.predict(X_val_2)}
+df = pd.DataFrame.from_dict(pred_dict)
+df.to_csv(val_pred_saving_path_2, index=False)
+
+perf_2 = {"eval_result": eval_result_2,
+          "recall_in_sample": recall_in_sample_2,
+          "recall_val": recall_val_2,
+          "trueNegRate_in_sample": trueNegRate_in_sample_2,
+          "trueNegRate_val": trueNegRate_val_2,
+          "thresholds for metric computation": [0.25, 0.5, 0.75]}
+with open(perf_saving_path_2, "w") as f:
+    f.write(str(perf_2))
+
+
+eval_result_3 = {}
+gbm_3 = lgb.train(
+    params,
+    lgb_train_3,
+    num_boost_round=10,
+    valid_sets=lgb_val_3,
+    callbacks=[lgb.record_evaluation(eval_result_3)]
+)
+"""gbm_3.save_model(model_saving_path_3, num_iteration=0)"""
+
+with open(model_saving_path_3, "wb") as fout:
+    pickle.dump(gbm_3, fout)
+
+recall_in_sample_3 = metric_individual_class_accuracy(y_true=y_train_3,
+                                                      y_pred=gbm_3.predict(X_train_3))
+recall_val_3 = metric_individual_class_accuracy(y_true=y_val_3,
+                                                y_pred=gbm_3.predict(X_val_3))
+
+trueNegRate_in_sample_3 = metric_individual_class_accuracy(y_true=y_train_3,
+                                                      y_pred=gbm_3.predict(X_train_3),
+                                                      which_class=0)
+trueNegRate_val_3 = metric_individual_class_accuracy(y_true=y_val_3,
+                                                      y_pred=gbm_3.predict(X_val_3),
+                                                      which_class=0)
+
+pred_dict = {"y_true": y_val_3, "y_pred": gbm_1.predict(X_val_3)}
+df = pd.DataFrame.from_dict(pred_dict)
+df.to_csv(val_pred_saving_path_3, index=False)
+
+perf_3 = {"eval_result": eval_result_3,
+          "recall_in_sample": recall_in_sample_3,
+          "recall_val": recall_val_3,
+          "trueNegRate_in_sample": trueNegRate_in_sample_3,
+          "trueNegRate_val": trueNegRate_val_3,
+          "thresholds for metric computation": [0.25, 0.5, 0.75]}
+with open(perf_saving_path_3, "w") as f:
+    f.write(str(perf_3))
+
+
+
+"""
+print("start ROI training based on all available training data...")
+policy_train = load_policy_training_data(repo_root)
+policy_test = load_policy_test_data(repo_root)
+
+X_train, y_train = get_policy_X_y_data(policy_train)
+W_train = add_vanilla_weights(policy_train, "label_invalid")
+
+X_test, y_test = get_policy_X_y_data(policy_test)
+W_test = add_vanilla_weights(policy_test, "label_invalid")
+
+# TODO: finish the train dataset below:
+lgb_train = lgb.Dataset()
+
+lgb_eval = lgb.Dataset(
+    X_test, y_test, reference=lgb_train_1, weight=W_test, free_raw_data=True
+)
+"""
---
+++ exec/training_ROI_pipe_0114.py
@@ -0,0 +1,178 @@
+import pickle
+import yaml
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+import catboost as cat
+
+import os
+import sys
+root = os.path.dirname(os.getcwd())
+repo_root = os.path.join(root, "meituan-coupon-roi")
+src_path = os.path.join(repo_root, "src")
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+from train import *
+from io_load import *
+id_param = "0115"
+
+date = "0115"
+m_id = "m_7d"
+
+seg_ids = ["seg_001", "seg_011_and_021", "seg_010", "seg_000", "oth_segs"]
+seg_params = []
+for p in range(3):
+    for c in range(3):
+        for e in range(2):
+            if [p, c, e] not in [[0, 0, 1], [0, 1, 1], [0, 1, 0], [0, 2, 1], [0, 0, 0]]:
+                seg_params.append([p, c, e])
+segs = {"seg_001": [[0, 0, 1]],
+        "seg_011_and_021": [[0, 1, 1], [0, 2, 1]],
+        "seg_010": [[0, 1, 0]],
+        "seg_000": [[0, 0, 0]],
+        "oth_segs": seg_params}
+
+params_yaml_path = os.path.join(repo_root, f"conf/params_ROI_{id_param}.yaml")
+selected_cols_pkl = f"conf/feature_selection/ROI_modeling/ROI_train_cols_{m_id}.pkl"
+test_data = "meituan-coupon-roi/data_work/processed/ROI_test_set_w_netprofit.parquet"
+
+perf_saving_path = os.path.join(repo_root, f"experiment_results/ROI/perf_ROI_{date}_{m_id}.txt")
+test_pred_saving_path = os.path.join(repo_root, f"experiment_results/ROI/test_pred_ROI_{date}_{m_id}.csv")
+models_saving_paths = {}
+for seg_id in seg_ids:
+    models_saving_paths[seg_id] = os.path.join(repo_root, f"models/ROI/ROI_{date}_{m_id}_{seg_id}.json")
+
+for seg_id in seg_ids:
+    print(f"Loading data for ROI training on segment {seg_id}...")
+    ROI_train = load_ROI_TrainorTest_data(repo_root,
+                                    pickle_path=selected_cols_pkl,
+                                    segment=True,
+                                    Price_limit_bin=segs[seg_id][0][0],
+                                    Coupon_limit_bin=segs[seg_id][0][1],
+                                    Expiry_span_bin=segs[seg_id][0][2])
+    ROI_test = load_ROI_TrainorTest_data(repo_root,
+                                    pickle_path=selected_cols_pkl,
+                                    data_set_path=test_data,
+                                    segment=True,
+                                    Price_limit_bin=segs[seg_id][0][0],
+                                    Coupon_limit_bin=segs[seg_id][0][1],
+                                    Expiry_span_bin=segs[seg_id][0][2])
+    ROI_test_bus_col = load_df_from_pq(test_data,
+                                       cols=["Actual_pay_cent"],
+                                       filters=[[("Price_limit_bin", "==", segs[seg_id][0][0]),
+                                                ("Coupon_limit_bin", "==", segs[seg_id][0][1]),
+                                                ("Expiry_span_bin", "==", segs[seg_id][0][2])]])["Actual_pay_cent"]
+    
+    if len(segs[seg_id]) > 1:
+        for seg_param in segs[seg_id][1:]:
+            train = load_ROI_TrainorTest_data(repo_root,
+                                        pickle_path=selected_cols_pkl,
+                                        segment=True,
+                                        Price_limit_bin=seg_param[0],
+                                        Coupon_limit_bin=seg_param[1],
+                                        Expiry_span_bin=seg_param[2])
+            test = load_ROI_TrainorTest_data(repo_root,
+                                        pickle_path=selected_cols_pkl,
+                                        data_set_path=test_data,
+                                        segment=True,
+                                        Price_limit_bin=seg_param[0],
+                                        Coupon_limit_bin=seg_param[1],
+                                        Expiry_span_bin=seg_param[2]
+                                        )
+            test_bus_col = load_df_from_pq(test_data, 
+                                           cols=["Actual_pay_cent"],
+                                           filters=[[("Price_limit_bin", "==", seg_param[0]),
+                                                     ("Coupon_limit_bin", "==", seg_param[1]),
+                                                      ("Expiry_span_bin", "==", seg_param[2])]])["Actual_pay_cent"]
+            
+            ROI_train = pd.concat([ROI_train, train], ignore_index=True)
+            ROI_test = pd.concat([ROI_test, test], ignore_index=True)
+            ROI_test_bus_col = pd.concat([ROI_test_bus_col, test_bus_col], ignore_index=True)
+
+    X_train, y_train = get_ROI_X_y_data(ROI_train, "st")
+    X_test, y_test = get_ROI_X_y_data(ROI_test, "st")
+    W_train = add_vanilla_weights(ROI_train, "label_same_user_st").to_numpy(dtype="float64")
+    W_test = add_vanilla_weights(ROI_test, "label_same_user_st").to_numpy(dtype="float64")
+
+    train_pool = cat.Pool(
+        data=X_train,
+        label=y_train,
+        cat_features=["Weekday_marker"],
+        weight=W_train)
+    
+    test_pool = cat.Pool(
+        data=X_test,
+        label=y_test,
+        cat_features=["Weekday_marker"],
+        weight=W_test
+    )
+
+    with open(params_yaml_path, 'r') as f:
+        params = yaml.safe_load(f)
+    
+    print(f"starting ROI training on the segment {seg_id}...")
+    model = cat.CatBoostClassifier(**params)
+    model.fit(train_pool, eval_set=train_pool, verbose=True)
+    eval_result = model.eval_metrics(train_pool, metrics=["Logloss"])
+
+    # save the model:
+    model.save_model(
+        models_saving_paths[seg_id],
+        format="json",
+        pool=train_pool
+    )
+
+    # compute metrics and save:
+    sigmoid = lambda x: 1/(1 + np.exp(-x))
+    y_pred_insample_raw = model.predict(X_train, 
+                                    prediction_type="RawFormulaVal")
+    y_pred_insample = sigmoid(y_pred_insample_raw)
+
+    y_pred_test_raw = model.predict(X_test,
+                                prediction_type="RawFormulaVal")
+    y_pred_test = sigmoid(y_pred_test_raw)
+
+    recall_insam = metric_individual_class_accuracy(y_true=y_train,
+                                                    y_pred=y_pred_insample)
+    recall_test = metric_individual_class_accuracy(y_true=y_test,
+                                                   y_pred=y_pred_test)
+    trueNegRate_insam = metric_individual_class_accuracy(y_true=y_train,
+                                                    y_pred=y_pred_insample,
+                                                    which_class=0)
+    trueNegRate_test = metric_individual_class_accuracy(y_true=y_test,
+                                                   y_pred=y_pred_test,
+                                                   which_class=0)
+    top5redemRate, top5profit = metric_business_top5profit(y_true=y_test,
+                                                    y_pred=y_pred_test,
+                                                    profit=ROI_test_bus_col)
+    profit_diff_thrshld = metric_business_profit_at_diff_thrshld(
+                                                    y_pred=y_pred_test,
+                                                    profit=ROI_test_bus_col)
+    perf = {"model for segment": seg_id,
+            "eval_result": eval_result,
+            "recall_in_sample": recall_insam,
+            "recall_in_test": recall_test,
+            "trueNegRate_in_sample": trueNegRate_insam,
+            "trueNegRate_in_test": trueNegRate_test,
+            "redemption rate of coupons with top raw prediction scores": top5redemRate,
+            "net profit per coupon with top raw prediction scores": top5profit,
+            "net profit per predicted-as-redeemed coupon at different thresholds": profit_diff_thrshld,
+            "thresholds for metric computation": [0.25, 0.5, 0.75]}
+
+    with open(perf_saving_path, "a") as f:
+        f.write(str(perf))
+    
+    # record predictions
+    pred_dict = {"y_true": y_test,
+                 "Actual_pay_cent": ROI_test_bus_col,
+                 "y_pred": y_pred_test}
+    if seg_id == "seg_001":
+        df = pd.DataFrame.from_dict(pred_dict)
+    else:
+        df_tmp = pd.DataFrame.from_dict(pred_dict)
+        df = pd.concat([df, df_tmp], ignore_index=True)
+
+df.to_csv(test_pred_saving_path, index=False)
